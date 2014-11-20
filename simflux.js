8a20cf16c8d2a698eb9f5de1f55748fccc432f8e
Gil Birman <gbirman@iseatz.com>
2014-11-20 00:35:07 -0600

    rename SimDispatcher-->SimfluxDispatcher

--- simflux.js
+++ simflux.js
@@ -1,17 +1,17 @@
 (function() {
 
-  var SimDispatcher = function() {
+  var SimfluxDispatcher = function() {
     this.fluxDispatcher = new Flux.Dispatcher();
   };
 
-  SimDispatcher.prototype.registerStore = function(store) {
+  SimfluxDispatcher.prototype.registerStore = function(store) {
     store.$$$dispatcherToken = this.fluxDispatcher.register(function(payload) {
       if (store[payload.action]) store[payload.action].apply(store, payload.args);
     });
     return store;
   };
 
-  SimDispatcher.prototype.unregisterStore = function(store) {
+  SimfluxDispatcher.prototype.unregisterStore = function(store) {
     this.fluxDispatcher.unregister(store.$$$dispatcherToken);
     delete store.$$$dispatcherToken;
     return store;
@@ -22,7 +22,7 @@
   //
   // Original: dispatcher.dispatch({ type: 'ACTION_TYPE', data: {whatever:data} })
   //      New: dispatcher.dispatch('ACTION_TYPE', arg1, arg2, ...)
-  SimDispatcher.prototype.dispatch = function(action) {
+  SimfluxDispatcher.prototype.dispatch = function(action) {
     return this.fluxDispatcher.dispatch({
       action: action,
       args: Array.prototype.slice.call(arguments, 1)
@@ -30,7 +30,7 @@
   };
 
   // waitFor takes a list of stores instead of tokens
-  SimDispatcher.prototype.waitFor = function (stores) {
+  SimfluxDispatcher.prototype.waitFor = function (stores) {
     var tokens = [];
     for (var i=0; i<stores.length; i++) {
       tokens.push(stores[i].$$$dispatcherToken);
@@ -39,13 +39,13 @@
   };
 
   // todo: use prototypical inheritance instead
-  SimDispatcher.prototype.isDispatching=function() {
+  SimfluxDispatcher.prototype.isDispatching=function() {
     return this.fluxDispatcher.isDispatching();
   };
 
   window.simflux = {
     version: 'pre-beta',
-    Dispatcher: SimDispatcher
+    Dispatcher: SimfluxDispatcher
   };
 
 })();
